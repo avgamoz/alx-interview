3f4f2b2222ecfd370218f64027d210b0268983c0
AKINSOLA BABAJIDE AMOS <90686696+avgamoz@users.noreply.github.com>
2023-07-18 16:58:04 +0100

    0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,37 +1,61 @@
 #!/usr/bin/python3
-'''a script that reads stdin line by line and computes metrics'''
-
-
+"""
+Script that reads stdin line by line and computes metrics:
+Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status
+                code> <file size>
+"""
+import re
 import sys
+import traceback
+db = {}
+pattern = r"^((\d+(\.\d+){3})|\w+?)\s?\-\s?" +\
+    r"(\[\d+(\-\d+){2}\s\d+(\:\d+\.?(\d+)?)" +\
+    r"{2}\])\s(\"GET \/projects\/260 HTTP\/1.1\")" +\
+    r"\s(\w+)\s(\d+)$"
+code = [200, 301, 400, 401, 403, 404, 405, 500]
+sum = 0
 
-cache = {'200': 0, '301': 0, '400': 0, '401': 0,
-         '403': 0, '404': 0, '405': 0, '500': 0}
-total_size = 0
-counter = 0
 
-try:
-    for line in sys.stdin:
-        line_list = line.split(" ")
-        if len(line_list) > 4:
-            code = line_list[-2]
-            size = int(line_list[-1])
-            if code in cache.keys():
-                cache[code] += 1
-            total_size += size
-            counter += 1
+def print_stats():
+    """
+    Function that print stats about log
+    """
+    global sum
 
-        if counter == 10:
-            counter = 0
-            print('File size: {}'.format(total_size))
-            for key, value in sorted(cache.items()):
-                if value != 0:
-                    print('{}: {}'.format(key, value))
+    print("File size: {}".format(sum))
+    stcdor = sorted(db.items())
+    for key, value in stcdor:
+        print("{}: {:d}".format(key, value))
 
-except Exception as err:
-    pass
 
-finally:
-    print('File size: {}'.format(total_size))
-    for key, value in sorted(cache.items()):
-        if value != 0:
-            print('{}: {}'.format(key, value))
+if __name__ == "__main__":
+    count = 0
+    try:
+        for data in sys.stdin:
+            count += 1
+            log = data.rstrip()
+            matches = re.findall(pattern, log)
+            size = 0
+
+            if matches is not None and len(matches) > 0:
+                matches = list(matches[0])
+                size = int(matches[-1])
+                try:
+                    status = int(matches[-2])
+
+                    if status in code:
+                        if db.get(status) is None:
+                            db[status] = 0
+                        db[status] += 1
+                except ValueError:
+                    pass
+                sum += int(size)
+
+            if count == 10:
+                print_stats()
+                count = 0
+    except KeyboardInterrupt:
+        print_stats()
+        traceback.print_exc()
+    else:
+        print_stats()
