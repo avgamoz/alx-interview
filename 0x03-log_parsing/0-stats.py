b0adfe1251caa1c7a1bcdd4e172a0efab773e071
AKINSOLA BABAJIDE AMOS <90686696+avgamoz@users.noreply.github.com>
2023-07-18 17:13:11 +0100

    0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,61 +1,46 @@
 #!/usr/bin/python3
-"""
-Script that reads stdin line by line and computes metrics:
-Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status
-                code> <file size>
-"""
-import re
+
+""" script that reads stdin line by line and computes metrics """
+
 import sys
-import traceback
-db = {}
-pattern = r"^((\d+(\.\d+){3})|\w+?)\s?\-\s?" +\
-    r"(\[\d+(\-\d+){2}\s\d+(\:\d+\.?(\d+)?)" +\
-    r"{2}\])\s(\"GET \/projects\/260 HTTP\/1.1\")" +\
-    r"\s(\w+)\s(\d+)$"
-code = [200, 301, 400, 401, 403, 404, 405, 500]
-sum = 0
 
 
-def print_stats():
-    """
-    Function that print stats about log
-    """
-    global sum
-
-    print("File size: {}".format(sum))
-    stcdor = sorted(db.items())
-    for key, value in stcdor:
-        print("{}: {:d}".format(key, value))
+def printStatus(dic, size):
+    """ Prints information """
+    print("File size: {:d}".format(size))
+    for i in sorted(dic.keys()):
+        if dic[i] != 0:
+            print("{}: {:d}".format(i, dic[i]))
 
 
-if __name__ == "__main__":
-    count = 0
-    try:
-        for data in sys.stdin:
-            count += 1
-            log = data.rstrip()
-            matches = re.findall(pattern, log)
-            size = 0
+# sourcery skip: use-contextlib-suppress
+statusCodes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+               "404": 0, "405": 0, "500": 0}
 
-            if matches is not None and len(matches) > 0:
-                matches = list(matches[0])
-                size = int(matches[-1])
-                try:
-                    status = int(matches[-2])
+count = 0
+size = 0
 
-                    if status in code:
-                        if db.get(status) is None:
-                            db[status] = 0
-                        db[status] += 1
-                except ValueError:
-                    pass
-                sum += int(size)
+try:
+    for line in sys.stdin:
+        if count != 0 and count % 10 == 0:
+            printStatus(statusCodes, size)
 
-            if count == 10:
-                print_stats()
-                count = 0
-    except KeyboardInterrupt:
-        print_stats()
-        traceback.print_exc()
-    else:
-        print_stats()
+        stlist = line.split()
+        count += 1
+
+        try:
+            size += int(stlist[-1])
+        except Exception:
+            pass
+
+        try:
+            if stlist[-2] in statusCodes:
+                statusCodes[stlist[-2]] += 1
+        except Exception:
+            pass
+    printStatus(statusCodes, size)
+
+
+except KeyboardInterrupt:
+    printStatus(statusCodes, size)
+    raise
